298ea8b0b3093f36b89eee858bcb8334cf443d0b
jagaucin2 <jagaucin2@miners.utep.edu>
2019-09-09 22:14:52 -0600

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BATT5',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Team5',
+    author_email='',
+    description=''
+)
