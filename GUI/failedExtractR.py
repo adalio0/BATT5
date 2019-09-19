f0e523b79556f14baa978b28b45e2bf39cc5773d
andrealabrado1 <35856984+andrealabrado1@users.noreply.github.com>
2019-09-19 17:10:18 -0600

    popups & errors

---
+++ GUI/failedExtractR.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'failedExtractR.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(336, 79)
+        self.gridLayout = QtWidgets.QGridLayout(Form)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+        self.okExtract = QtWidgets.QPushButton(Form)
+        self.okExtract.setObjectName("okExtract")
+        self.gridLayout.addWidget(self.okExtract, 2, 0, 1, 1, QtCore.Qt.AlignRight)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "                     Error: Failed to extract properties"))
+        self.label_2.setText(_translate("Form", "                                  Error From Radare2"))
+        self.okExtract.setText(_translate("Form", "Ok"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
---
+++ GUI/uiControl.py
@@ -0,0 +1,22 @@
+
+import sys
+from PyQt5 import QtWidgets
+from ui import Ui_MainWindow
+
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(ApplicationWindow, self).__init__()
+        self.window = Ui_MainWindow()
+        self.window.setupUi(self)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    application = ApplicationWindow()
+    application.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
