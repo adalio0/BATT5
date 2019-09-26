c727c6fc641795e07a0eb534a3fc42fbee3582d3
Alain Sanchez <salain919@gmail.com>
2019-09-25 19:28:27 -0600

    XML editor integrated, file selector integrated

--- src/Functionality/uiControl.py
+++ src/Functionality/uiControl.py
@@ -1,11 +1,18 @@
 #! /usr/bin/env python3.
 
+import os
 import sys
 import subprocess
+import xml.etree.ElementTree as ET
+
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
 from src.GUI.python_files.BATT5_GUI import Ui_BATT5
-from src.GUI.python_files.popups.errors import ErrFile, Errx86, ErrBFile
+from src.GUI.python_files.popups.errors import ErrFile, Errx86, ErrRadare
 from src.GUI.python_files.popups.newProjectWind import NewProject
+from src.GUI.python_files.popups.xmlEditor import XMLEditor
 
 
 class ApplicationWindow(QtWidgets.QMainWindow):
@@ -14,24 +21,37 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.window = Ui_BATT5()
         self.window.setupUi(self)
 
-        # Clicking on browse button calls showFileExplorer method
+        # Clicking on Plugin Structure browse button calls showFileExplorer method
         self.window.dpmPluginStructure_button.clicked.connect(self.showFileExplorer)
 
-        # Clicking on browse button calls showErrFile method
-        self.window.commentSave_button.clicked.connect(self.showErrFile)
+        # Clicking on Plugin Predefined browse button calls showFileExplorer method (xmlEditor for now)
+        self.window.dpmPluginPredefined_button.clicked.connect(self.xmlEditor)
 
-        # Clicking on New.. menu bar calls showFileExplorer method
+        # Clicking on New.. menu bar calls showNewProject method
         self.window.actionNew_Project.triggered.connect(self.showNewProject)
+
+    # Opens up an xml (file) editor TODO: If we open this window it creates a bug where we can't select a file in the
+    # TODO:                                 main window need to figure out a fix.
+    def xmlEditor(self):
+        self.window = XMLEditor()
+        self.window.show()
+
+        cur_path = os.getcwd()
+        file = os.path.join(cur_path, '..', 'Configurations', 'country_data.xml')
+        tree = ET.parse(file)
+        root = tree.getroot()
+
+        # print(root.tag)
 
     # Shows NewProject window TODO: There's a bug here can't figure it out.
     def showNewProject(self):
         self.window = NewProject()
-        # self.window.setupUi(self)
         # self.window.show()
 
-    # Should open up file explorer TODO: Don't know if it actually works, need to integrate file selection.
+    # Open up file explorer to select a file
     def showFileExplorer(self):
-        subprocess.Popen(r'explorer')
+        name, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File')
+        self.window.dpmPluginStructure_lineEdit.setText(name)
 
     # Shows ErrFile window
     def showErrFile(self):
@@ -45,7 +65,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
     # Shows ErrBFile window
     def showErrBFile(self):
-        self.window = ErrBFile()
+        self.window = ErrRadare()
         self.window.show()
 
 
