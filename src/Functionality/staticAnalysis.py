a9069529a82c585e50bdb64dc4f9cebc243c3fa5
jagaucin2 <jagaucin2@miners.utep.edu>
2019-10-08 16:48:50 -0600

    -additional test on integration

---
+++ src/Functionality/staticAnalysis.py
@@ -0,0 +1,8 @@
+import r2pipe
+import json
+import re
+def staticAnalysis(filePath,poi):
+    infile = r2pipe.open(filePath)
+    imports = infile.cmd(poi)
+    formatted = json.loads(imports) # formats json output into a python dictionary
+    return formatted
