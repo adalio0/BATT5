d7cc61d55f45ce5a8ca07954ff36df43cbc5e62d
Alain Sanchez <salain919@gmail.com>
2019-10-10 17:33:46 -0600

    Adding functionality on menu bar, rewored xml editor but is not finished yet.

--- src/Functionality/xmlTesting.py
+++ src/Functionality/xmlTesting.py
@@ -1,25 +1,22 @@
 import os
 import xml.etree.ElementTree as ET
 
-from pathlib import Path
-
 
 def xmlParser():
     cur_path = os.getcwd()
-    file = os.path.join(cur_path, '..', 'Configurations', 'country_data.xml')
+    file = os.path.join(cur_path, '..', 'Configurations', 'project.xml')
     tree = ET.parse(file)
     root = tree.getroot()
 
     print(root.tag)
     for child in root:
-        print(child.tag, child.attrib)
+        print(child.tag, child.attrib, child[0].text)
 
-    print(root[0][0].text)
+    for element in root.iter('os'):
+        element.set('content', 'Linux')
 
-    for rank in root.iter('rank'):
-        new_rank = int(rank.text) + 1
-        rank.text = str(new_rank)
-        rank.set('updated', 'yes')
+    value = root.find('.//project/os[@name="{}"]'.format('Linux')).text
+    print(value)
 
     tree.write(file)
 
