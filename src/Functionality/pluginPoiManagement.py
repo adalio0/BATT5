4bcfa999c8be300819577682315566e3fbc3fe12
adalio0 <43177494+adalio0@users.noreply.github.com>
2019-11-05 18:01:22 -0700

    line added (stashing)

--- src/Functionality/pluginPoiManagement.py
+++ src/Functionality/pluginPoiManagement.py
@@ -87,4 +87,5 @@ def processPluginData(createType, dpmPluginStructure_lineEdit, dpmPluginName_lin
                                          dpmOutFuncSource_lineEdit.text())
     print(pluginDict)
     return pluginDict
+
 # TODO make function to store into db
