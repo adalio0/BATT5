499c9e2a7fe29f77b886e32ffe6d6814f3cc9043
Alain Sanchez <salain919@gmail.com>
2019-11-22 20:52:56 -0700

    Added a very OP documentation tab. (Probably didn't need all of it but it was fun and its cool)

--- src/Functionality/documentation.py
+++ src/Functionality/documentation.py
@@ -7,9 +7,7 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMessageBox, QFileDialog
 
 from src.GUI.python_files.popups.documentationView import Documentation_Window
-from src.GUI.python_files.popups.fileEditor import FileEditor
-
-edit = False
+from src.Functionality.search import searchDocumentation
 
 
 class DocumentationWindow(QtWidgets.QDialog):
@@ -17,35 +15,29 @@ class DocumentationWindow(QtWidgets.QDialog):
         super(DocumentationWindow, self).__init__()
         self.window = Documentation_Window()
         self.window.setupUi(self)
-        self.populateDoc()
 
+        self.populateDoc()
         self.window.saveButton.setVisible(False)
         self.window.dDocumentView_textEdit.setReadOnly(True)
 
-        self.window.documentView_listWidget.itemSelectionChanged.connect(self.show)
+        self.window.documentView_lineEdit.textChanged.connect(self.callSearchDocumentation)
+        self.window.documentView_listWidget.itemSelectionChanged.connect(self.showFile)
+
         self.window.addButton.clicked.connect(self.showFileExplorer)
+        self.window.deleteButton.clicked.connect(self.delete)
+        self.window.editButton.setShortcut('Ctrl+E')
         self.window.editButton.clicked.connect(self.edit)
+        self.window.saveButton.setShortcut('Ctrl+S')
         self.window.saveButton.clicked.connect(self.save)
 
-    # ---- Methods ------------------------------------------------------------------------------
+    # ---- Main methods for the window --------------------------------------------------------------------------
 
     def populateDoc(self):
         path = Path(__file__).parents[2].as_posix() + '/Documentation/'
         for file in glob.glob(path + "/**/" + '*.txt', recursive=True):
             self.window.documentView_listWidget.addItem(file.split(path)[-1].split('.txt')[0])
 
-    def save(self):
-        name, _ = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File', options=QFileDialog.DontUseNativeDialog)
-
-        try:
-            file = open(name, 'w')
-            text = self.window.dDocumentView_textEdit.toPlainText()
-            file.write(text)
-            file.close()
-        except FileNotFoundError:
-            pass
-
-    def show(self):
+    def showFile(self):
         self.window.dDocumentView_textEdit.clear()
         if self.window.documentView_listWidget.currentItem():
             name = self.window.documentView_listWidget.currentItem().text()
@@ -60,34 +52,77 @@ class DocumentationWindow(QtWidgets.QDialog):
             except FileNotFoundError:
                 pass
 
+    # ---- Search functionality ----------------------------------------------------------------------------------
+
+    def callSearchDocumentation(self):
+        try:
+            searchDocumentation(self.window.documentView_lineEdit.text(), self.window.documentView_listWidget)
+        except AttributeError:
+            pass
+
+    # ---- Save, Delete, Edit methods -----------------------------------------------------------------------------
+
+    def save(self):
+        path = Path(__file__).parents[2].as_posix() + '/Documentation/'
+        name = self.window.documentView_listWidget.currentItem().text()
+
+        try:
+            file = open((path + name + '.txt'), 'w')
+            text = self.window.dDocumentView_textEdit.toPlainText()
+            file.write(text)
+            file.close()
+        except FileNotFoundError:
+            pass
+
+    def delete(self):
+        path = Path(__file__).parents[2].as_posix() + '/Documentation/'
+        name = self.window.documentView_listWidget.currentItem().text()
+
+        if os.path.exists(path + name + ".txt"):
+            os.remove(path + name + ".txt")
+        else:
+            print("The file does not exist")
+
+        self.window.documentView_listWidget.clear()
+        self.populateDoc()
+
     def edit(self):
-        global edit
-        if not edit:
+        if self.window.editButton.text() == "Edit":
             self.window.saveButton.setVisible(True)
             self.window.editButton.setText("Done")
             self.window.dDocumentView_textEdit.setReadOnly(False)
-            edit = True
-            self.show()
+            self.showFile()
         else:
             self.window.saveButton.setVisible(False)
             self.window.editButton.setText("Edit")
             self.window.dDocumentView_textEdit.setReadOnly(True)
-            edit = False
 
-    def showFileExplorer(self):
-        options = QtWidgets.QFileDialog.Options()
-        options |= QtWidgets.QFileDialog.DontUseNativeDialog
-        name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Browse", "",
-                                                        "All Files (*);;Text Files (*.txt)", options=options)
-        print(name)
-        self.window.documentView_listWidget.addItem(name)
-
-    # ---- Show Error Message ------------------------------------------
+    # ---- Methods to call show windows --------------------------------------------------------------------------
 
     def showErr(self):
         QMessageBox.question(self, "Error Message: Missing Fields",
                              "All fields must be filled to in order to create a Project",
                              QMessageBox.Ok)
+
+    def showFileExplorer(self):
+        path = Path(__file__).parents[2].as_posix() + '/Documentation/'
+
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        name, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Add File", "",
+                                                        "All Files (*);;Text Files (*.txt)", options=options)
+
+        try:
+            file = open((path + name.split('/')[-1].split('.')[0] + '.txt'), 'w+')
+            contents = open(name, 'r')
+            file.write(contents.read())
+            contents.close()
+            file.close()
+            self.window.documentView_listWidget.clear()
+            self.populateDoc()
+        except UnicodeDecodeError:
+            print('nah bitch')
+            pass
 
 
 def main():
