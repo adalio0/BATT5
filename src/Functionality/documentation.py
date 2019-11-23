e07e273384cea863577456dc526e2d175cd4e022
marknunez97 <marknunez1997@gmail.com>
2019-11-22 19:08:43 -0700

    removed temp list items

--- src/Functionality/documentation.py
+++ src/Functionality/documentation.py
@@ -4,7 +4,6 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 
 from src.GUI.python_files.popups.documentationView import Documentation_Window
-from src.GUI.python_files.popups.errors import ErrEmptyFields
 
 edit = False
 
