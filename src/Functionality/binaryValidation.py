375de6b1eb102652e7fab0867b17b4e0371c8171
JuanGaucin <jagaucinc@gmail.com>
2019-11-26 19:30:40 -0800

    -refactor binary validation

--- src/Functionality/binaryValidation.py
+++ src/Functionality/binaryValidation.py
@@ -4,5 +4,6 @@ import re
 
 def get_binary_info(filepath):
     infile = r2pipe.open(filepath)
-    infile.syscmd("rabin2 -I " + filepath + "  > binaryinfo.txt")
+    fileProperties = infile.cmd("ij")
+    return fileProperties
 
