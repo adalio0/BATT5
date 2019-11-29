abc57bcfe1e384b1e8558d3bd64ce82c401246c0
adalio0 <adal.rivas317@gmail.com>
2019-11-29 12:55:31 -0700

    new project ui file name fix

--- src/GUI/python_files/popups/newProjectWind.py
+++ src/GUI/python_files/popups/newProjectWind.py
@@ -10,7 +10,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Form(object):
+class NewProject(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(533, 503)
