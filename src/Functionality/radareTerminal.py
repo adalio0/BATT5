356512a762442e485dc7feb21b13f78cf209df69
adalio0 <adal.rivas317@gmail.com>
2019-12-03 01:11:31 -0700

    Terminal outputs prompt to select project if there is no project selected.

--- src/Functionality/radareTerminal.py
+++ src/Functionality/radareTerminal.py
@@ -27,7 +27,10 @@ class Terminal(object):
             outStr = 'Analyzing binary: ' + self.binaryPath + '\n'
 
         except:
-            outStr = 'Error opening binary: ' + self.binaryPath + '\n'
+            if self.binaryPath == '':
+                outStr = 'Please select a project to begin analyzing a binary.\n'
+            else:
+                outStr = 'Error opening binary: ' + self.binaryPath + '\n'
 
         finally:
             self.promptOut.insertPlainText(outStr)
